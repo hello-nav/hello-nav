475ea9070bb6121fcf2da0316848486e49fee9d6
Bener <2388697970@qq.com>
2018-12-08 00:10:14 +0800

    add flutter && build

--- src/mock/modules/06-other.js
+++ src/mock/modules/06-other.js
@@ -17,4 +17,9 @@ export default [{
   imgSrc: requireImg('dart.png'),
   keyWords: ['dart'],
   label: 'Dart'
+}, {
+  url: 'https://flutter.io/',
+  imgSrc: requireImg('flutter.png'),
+  keyWords: ['flutter'],
+  label: 'Flutter'
 }]
