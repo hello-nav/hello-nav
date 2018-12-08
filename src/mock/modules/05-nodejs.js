2d5161b8187dd252c25fd897d79b30258b1eb597
Bener <2388697970@qq.com>
2018-12-09 00:18:30 +0800

    add koa && build

--- src/mock/modules/05-nodejs.js
+++ src/mock/modules/05-nodejs.js
@@ -5,16 +5,21 @@ import {
 export default [{
   url: 'http://nodejs.cn/api/',
   imgSrc: requireImg('nodejs.png'),
-  keyWords: ['nodejs'],
+  keyWords: ['nodejs', 'node'],
   label: 'NodeJS'
 }, {
   url: 'http://expressjs.com/',
   imgSrc: requireImg('express.png'),
-  keyWords: ['express'],
+  keyWords: ['express', 'node'],
   label: 'Express'
+}, {
+  url: 'https://koajs.com/',
+  imgSrc: requireImg('koa.png'),
+  keyWords: ['koa', 'node'],
+  label: 'Koa'
 }, {
   url: 'https://eggjs.org/',
   imgSrc: requireImg('egg.png'),
-  keyWords: ['egg'],
+  keyWords: ['egg', 'node'],
   label: 'Egg'
 }]
