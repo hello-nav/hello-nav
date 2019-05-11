e78d86583efd83d36bf42bb2742e059e090897f7
ben <2388697970@qq.com>
2019-05-11 11:34:02 +0800

    add ng-alain

--- src/mock/modules/02-ui-library.js
+++ src/mock/modules/02-ui-library.js
@@ -5,38 +5,43 @@ import {
 export default [{
   url: 'https://element.eleme.cn/',
   imgSrc: requireImg('element-ui.png'),
-  keyWords: ['饿了么', 'element', 'element-ui', 'ui'],
+  keyWords: ['饿了么', 'element', 'element-ui'],
   label: 'Element'
 }, {
   url: 'https://www.iviewui.com/',
   imgSrc: requireImg('iview.png'),
-  keyWords: ['iview', 'ui'],
+  keyWords: ['iview'],
   label: 'iView'
 }, {
   url: 'https://muse-ui.org/',
   imgSrc: requireImg('museui.png'),
-  keyWords: ['muse-ui', 'ui'],
+  keyWords: ['muse-ui'],
   label: 'Muse-ui'
 }, {
   url: 'https://at-ui.github.io/at-ui/',
   imgSrc: requireImg('at-ui.png'),
-  keyWords: ['at-ui', 'ui'],
+  keyWords: ['at-ui'],
   label: 'At-ui'
 }, {
   url: 'https://youzan.github.io/vant',
   imgSrc: requireImg('vant.png'),
-  keyWords: ['vant', 'ui'],
+  keyWords: ['vant'],
   label: 'Vant'
 }, {
   url: 'https://ant.design/',
   imgSrc: requireImg('ant-design.png'),
-  keyWords: ['antd', 'ant-design', 'ui'],
+  keyWords: ['antd', 'ant-design'],
   label: 'Ant-Design'
 }, {
   url: 'https://antv.alipay.com/',
   imgSrc: requireImg('antv.png'),
-  keyWords: ['antv', 'ui'],
+  keyWords: ['antv'],
   label: 'AntV'
+}, {
+  url: 'https://ng-alain.com/zh',
+  imgSrc: requireImg('ng-alain.png'),
+  keyWords: ['ng-alain'],
+  label: 'Ng-Alain'
 }, {
   url: 'http://www.iconfont.cn/',
   imgSrc: requireImg('iconfont.png'),
