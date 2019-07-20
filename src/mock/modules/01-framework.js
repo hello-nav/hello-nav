e09a436519fba68d80fa16330dbb7bcfff893c13
ben <2388697970@qq.com>
2019-07-20 10:29:46 +0800

    update github url

--- src/mock/modules/01-framework.js
+++ src/mock/modules/01-framework.js
@@ -28,47 +28,54 @@ export default [{
   label: 'Angular'
 }, {
   url: 'https://preactjs.com/',
-  githubUrl: 'https://github.com/angular/angular',
+  githubUrl: 'https://github.com/preactjs/preact',
   imgSrc: requireImg('preactjs.png'),
   keyWords: ['preactjs'],
   label: 'Preactjs'
 }, {
   url: 'https://nerv.aotu.io/',
+  githubUrl: 'https://github.com/NervJS/nerv',
   imgSrc: requireImg('nervjs.png'),
   keyWords: ['nervjs'],
   label: 'NervJS'
 }, {
   url: 'https://cml.js.org/#/',
+  githubUrl: 'https://github.com/didi/chameleon',
   imgSrc: requireImg('chameleon.png'),
   keyWords: ['chameleon'],
   label: 'Chameleon'
 }, {
   url: 'https://nuxtjs.org/',
+  githubUrl: 'https://github.com/nuxt/nuxt.js',
   imgSrc: requireImg('nuxt.png'),
   keyWords: ['nuxt', 'vue'],
   label: 'Nuxt'
 }, {
   url: 'https://nextjs.org/',
+  githubUrl: 'https://github.com/zeit/next.js',
   imgSrc: requireImg('next.png'),
   keyWords: ['next', 'react'],
   label: 'Next'
 }, {
   url: 'https://electronjs.org/',
+  githubUrl: 'https://github.com/electron/electron',
   imgSrc: requireImg('electron.png'),
   keyWords: ['electron'],
   label: 'Electron'
 }, {
   url: 'https://weex.apache.org/',
+  githubUrl: 'https://github.com/apache/incubator-weex',
   imgSrc: requireImg('weex.png'),
   keyWords: ['weex', 'vue'],
   label: 'Weex'
 }, {
   url: 'https://facebook.github.io/react-native/',
+  githubUrl: 'https://github.com/facebook/react-native',
   imgSrc: requireImg('react.png'),
   keyWords: ['react-native', 'react', 'rn'],
   label: 'React-Native'
 }, {
-  url: 'https://mp.weixin.qq.com/debug/wxadoc/dev/framework/MINA.html',
+  url: 'https://developers.weixin.qq.com/miniprogram/dev/framework/',
   imgSrc: requireImg('xcx.png'),
   keyWords: ['微信小程序', 'weixin', 'xiaochengxu'],
   label: '微信小程序'
