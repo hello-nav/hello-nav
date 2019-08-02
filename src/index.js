d2497f7f98fa047621673d5f379d8aef8e7b0882
GA_FE <123@456.com>
2019-08-02 15:13:45 +0800

    refactor with latest react

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom'
 import initIconfont from '../src/assets/fonts/iconfont'
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker'
 initIconfont()
-ReactDOM.render(<App />, document.getElementById('app'))
-registerServiceWorker()
+ReactDOM.render(<App/>, document.getElementById('app'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
+
