5c94556d82adfa052aba74eb7e718d6973df85bd
GA_FE <123@456.com>
2019-08-12 16:18:37 +0800

    add x link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,13 @@ import './index.css'
 const Footer = () => (
   <div className="footer__wrap">
     <p>
-      <span className="logo vertical-center"></span>
+      <a href="https://github.com/hello-nav/hello-nav" className="logo vertical-center"> </a>
       <span className="vertical-center">
-        Made by<b> Bener </b>with
+        Made by<b> <a className="bener-link" href="https://github.com/Heroor">Bener</a> </b>with
         <span className="heart"> ❤ </span>
       </span>
     </p>
-    <p>&copy; 2018-2019</p>
+    <p>&copy;2018-2019</p>
 
   </div>
 )
