c3ae73468fdb7b89fdef58909bfd76f9a912c60a
ben <2388697970@qq.com>
2019-09-22 21:20:05 +0800

    fix app handler

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Children } from 'react'
+import React, { useState } from 'react'
 import libraryTree from '../../mock'
 import FilterBar from '../FilterBar'
 import ContainWrap from '../Contain'
@@ -6,18 +6,21 @@ import WithError from '../WithError'
 import Footer from '../Footer'
 
 const CATEGORY_TYPES: CategoryTypes = ['list', 'category']
+const KEYWORD_REG = /[ -]/g
 const ContainWithNotFind = WithError(ContainWrap, 'Ooops! Can not find it here...')
+
+function transformAppKeyWords (app: AppItem): void {
+  if (!Array.isArray(app.keyWords) || !app.keyWords.length) {
+    app.keyWords = [app.label]
+  } else {
+    app.keyWords = app.keyWords.map(key => key.toLowerCase().replace(KEYWORD_REG, ''))
+  }
+}
 
 const libraryMap: LibraryMap = {
   category: libraryTree,
   list: libraryTree.reduce((res: AppItem[], item: CateItem) => {
-    item.childrens.forEach(app => {
-      if (!app.keyWords || !app.keyWords.length) {
-        app.keyWords = [app.label]
-      } else {
-        app.keyWords = app.keyWords.map(key => key.toLowerCase().replace(/[ -]/g, ''))
-      }
-    })
+    item.childrens.forEach(transformAppKeyWords)
     res.push(...item.childrens)
     return res
   }, [])
@@ -58,14 +61,15 @@ let toggleType = (setType: any, setList: any, type?: CategoryType) => {
 }
 
 function App () {
-  const [type, setType]: [CategoryType, React.Dispatch<any>] = useState(window.localStorage.__CATEGORY_TYPE__ || CATEGORY_TYPES[0])
-  !window.localStorage.__CATEGORY_TYPE__ && (window.localStorage.__CATEGORY_TYPE__ = type)
+  const __CATEGORY_TYPE__ = window.localStorage.__CATEGORY_TYPE__
+  const [type, setType]: [CategoryType, React.Dispatch<any>] = useState(__CATEGORY_TYPE__ || CATEGORY_TYPES[0])
+  !__CATEGORY_TYPE__ && (window.localStorage.__CATEGORY_TYPE__ = type)
 
   const [list, setList] = useState(libraryMap[type])
   const [filterKey, setFilterKey] = useState('')
   typeIndex = CATEGORY_TYPES.indexOf(type)
 
-  const newFilterKey = filterKey.trim().replace(/[ -]/g, '')
+  const newFilterKey = filterKey.trim().replace(KEYWORD_REG, '')
   if (oldFilterKey !== newFilterKey) {
     setList(listFiltersMap[type](newFilterKey))
     oldFilterKey = newFilterKey
