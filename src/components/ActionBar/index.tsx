baefd95eb00a266142c4ee4505aba0dae7771ac0
bener <2388697970@qq.com>
2020-04-12 13:21:45 +0800

    feat: onload with dark theme

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -2,23 +2,13 @@ import React from 'react'
 import PandaBtn from '../PandaBtn'
 import './index.css'
 
-const DARK_CLASS_NAME = 'theme--dark'
-function toggleTheme() {
-  const bodyClassList = document.body.classList
-  if (bodyClassList.contains(DARK_CLASS_NAME)) {
-    bodyClassList.remove(DARK_CLASS_NAME)
-  } else {
-    bodyClassList.add(DARK_CLASS_NAME)
-  }
-}
-
 function FilterBar({
   filterKey, onInput, toggleType, type,
 }: FilterProps) {
   return (
     <div className="filter-bar">
       <div className="filter-bar__btn">
-        <PandaBtn onClick={toggleTheme} />
+        <PandaBtn />
         <div className="filter-bar__toggle-btn" onClick={toggleType} onKeyDown={() => {}}>
           <svg className={`icon ${type}-icon`} aria-hidden="true">
             <use href={`#icon-${type}`} />
--- src/components/PandaBtn/index.tsx
+++ src/components/PandaBtn/index.tsx
@@ -5,14 +5,28 @@ interface PandaBtnProps {
   onClick?: (event: React.SyntheticEvent) => void;
 }
 
+const DARK_CLASS_NAME = 'theme--dark'
+
+function toggleBodyClassName(isDark: boolean) {
+  const bodyClassList = document.body.classList
+  if (isDark) {
+    bodyClassList.remove(DARK_CLASS_NAME)
+    window.localStorage.__THEME__ = ''
+  } else {
+    bodyClassList.add(DARK_CLASS_NAME)
+    window.localStorage.__THEME__ = DARK_CLASS_NAME
+  }
+}
+
 export default function PandaBtn({ onClick }: PandaBtnProps) {
-  const [status, setStatus] = useState(false)
+  const [status, setStatus] = useState(!!window.localStorage.__THEME__)
   const classNames = [
     'panda-btn',
-    status && 'theme--dark',
+    status && DARK_CLASS_NAME,
   ].join(' ')
 
   const handleClick: React.MouseEventHandler = e => {
+    toggleBodyClassName(status)
     setStatus(!status)
     if (onClick) {
       (onClick as React.MouseEventHandler)(e)
