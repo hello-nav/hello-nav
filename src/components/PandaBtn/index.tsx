a6dc648b870e979e3d9b4c778dc278ba70561d1b
Bener <2388697970@qq.com>
2021-11-19 17:39:06 +0800

    feat:  browser theme toggle

--- src/components/PandaBtn/index.tsx
+++ src/components/PandaBtn/index.tsx
@@ -7,7 +7,7 @@ interface PandaBtnProps {
 
 const STATUS_CLASS_NAME = 'theme--dark'
 
-function toggleBodyClassName(isStatusTrue: boolean) {
+function toggleTheme(isStatusTrue: boolean) {
   const pageClassList = document.documentElement.classList
   if (isStatusTrue) {
     pageClassList.remove(STATUS_CLASS_NAME)
@@ -15,6 +15,10 @@ function toggleBodyClassName(isStatusTrue: boolean) {
   } else {
     pageClassList.add(STATUS_CLASS_NAME)
     window.localStorage.__THEME__ = STATUS_CLASS_NAME
+  }
+  const themeMateEle = document.querySelector('meta[name="theme-color"]')
+  if (themeMateEle) {
+    themeMateEle.setAttribute('content', isStatusTrue ? '#f6f7f9' : 'rgba(45, 46, 48, 0.9)')
   }
 }
 
@@ -26,7 +30,7 @@ export default function PandaBtn({ onClick }: PandaBtnProps) {
   ].join(' ')
 
   const handleClick: React.MouseEventHandler = e => {
-    toggleBodyClassName(status)
+    toggleTheme(status)
     setStatus(!status)
     if (onClick) {
       (onClick as React.MouseEventHandler)(e)
