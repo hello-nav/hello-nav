eff844f1227373e3cad9c5638eac6de1285965a0
Bener <benjavan@163.com>
2022-01-17 13:58:36 +0800

    fix: add '_' to the keyword regexp

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const IGNORE_KEYWORD_REG = /[ -.]/g
+export const IGNORE_KEYWORD_REG = /[ -_.]/g
 
 export function transformAppKeyWords(app: AppItem): void {
   if (!Array.isArray(app.keyWords) || !app.keyWords.length) {
