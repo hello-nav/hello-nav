8fc50e42a3bd268dd5d5a14b297d802feb508868
Bener <benjavan@163.com>
2024-04-25 00:42:58 +0800

    fix: encode filter key

--- src/components/WithError/Message.tsx
+++ src/components/WithError/Message.tsx
@@ -6,12 +6,12 @@ const Message = ({ filterKey }: { filterKey: string }) => (
     Oops! Couldn't find it here...
     <div className="error-tips">
       Try searching on{' '}
-      <a href={`https://github.com/search?q=${filterKey}`} target="_blank">
+      <a href={`https://github.com/search?q=${encodeURIComponent(filterKey)}`} target="_blank">
         <ReactSVG className="icon" width={18} height={18} src={gitHubIcon}></ReactSVG> GitHub
       </a>{' '}
       or{' '}
       <a
-        href="https://github.com/hello-nav/hello-nav/issues/new?assignees=Heroor&labels=request&projects=&template=request-app.md&title=Request+%28app-name%29"
+        href={`https://github.com/hello-nav/hello-nav/issues/new?assignees=Heroor&labels=request&projects=&template=request-app.md&title=Request ${encodeURIComponent(filterKey)}`}
         target="_blank"
       >
         Request App
