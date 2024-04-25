9cf5eea791f010c4e89c3aeaf6c18b9c99f44f38
Bener <benjavan@163.com>
2024-04-25 22:49:15 +0800

    feat: support chrome extension

---
+++ scripts/build-ext.mjs
@@ -0,0 +1,18 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import 'zx/globals'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const extensionDir = resolve(__dirname, '../src/extension')
+const distDir = resolve(__dirname, '../dist')
+const outputDir = resolve(__dirname, '../extension')
+
+await $`mkdir -p ${outputDir}`
+await $`rm -rf ${outputDir}/*`
+
+await $`cp -r ${distDir}/* ${outputDir}`
+await $`cp -rf ${extensionDir}/* ${outputDir}`
+
+console.log('🧩 Extension generation success!')
