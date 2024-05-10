f434b21c0ba8e105f22ebc7b762d4af30217c2e1
Bener <benjavan@163.com>
2024-05-11 03:24:30 +0800

    fix: modify head script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,13 @@
 import './src/styles/global.css'
 
 export const onClientEntry = () => {
-  if (document.documentElement) {
-    var themeClassName = window.localStorage.__THEME__ || 'none'
-    document.documentElement.className = themeClassName
-    if (themeClassName === 'theme--dark') {
-      document
-        .querySelector('meta[name="theme-color"]')
-        ?.setAttribute('content', 'rgba(45, 46, 48, 0.9)')
-    }
-  }
+  // if (document.documentElement) {
+  //   var themeClassName = window.localStorage.__THEME__ || 'none'
+  //   document.documentElement.className = themeClassName
+  //   if (themeClassName === 'theme--dark') {
+  //     document
+  //       .querySelector('meta[name="theme-color"]')
+  //       ?.setAttribute('content', 'rgba(45, 46, 48, 0.9)')
+  //   }
+  // }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,6 +49,19 @@ export const Head: HeadFC = () => {
           }
         `}
       </style>
+      <script>
+        {`
+          if (document.documentElement) {
+            var themeClassName = window.localStorage.__THEME__ || 'none'
+            document.documentElement.className = themeClassName
+            if (themeClassName === 'theme--dark') {
+              document
+                .querySelector('meta[name="theme-color"]')
+                ?.setAttribute('content', 'rgba(45, 46, 48, 0.9)')
+            }
+          }
+        `}
+      </script>
     </>
   )
 }
